6a804a42651fe30a8e955972089ee29485bde20f
Manu Farfaro <manufarfaro@gmail.com>
2016-11-18 13:46:04 -0300

    some javascript libs deprecated

--- app/assets/javascripts/application.net.js
+++ app/assets/javascripts/application.net.js
@@ -14,11 +14,11 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
-//= require jquery-locationpicker-plugin/dist/locationpicker.jquery
+// require jquery-locationpicker-plugin/dist/locationpicker.jquery
 //= require filterrific/filterrific-jquery
-//= require bootstrap-switch/dist/js/bootstrap-switch
+// require bootstrap-switch/dist/js/bootstrap-switch
 //= require matchHeight/jquery.matchHeight
 //= require choices.js/assets/scripts/dist/choices.js
-//= require ace-builds/src/ace
+// require ace-builds/src/ace
 //= require vanilla-datatables/vanilla-dataTables
 //= require net/app
